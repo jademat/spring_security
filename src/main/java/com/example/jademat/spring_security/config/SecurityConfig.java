cf77f9094aea643aa4b393e9819c562178f489bc
jademat <01088049128a@gmail.com>
2025-03-21 10:03:10 +0900

    passwordEncoder 수정

--- src/main/java/com/example/jademat/spring_security/config/SecurityConfig.java
+++ src/main/java/com/example/jademat/spring_security/config/SecurityConfig.java
@@ -25,22 +25,20 @@ public class SecurityConfig {
     public SecurityFilterChain FilterChain(HttpSecurity http, HttpServlet httpServlet, HttpServletResponse httpServletResponse) throws Exception {
         http
                 .csrf().disable()                                                   // CSRF 필터 끔
-                .authorizeHttpRequests()                                            // URL 기반 인가 설정
-                    .antMatchers("/user/**").hasRole("USER")
-                    .antMatchers("/admin/**").hasRole("ADMIN")
+            .authorizeHttpRequests()                                            // URL 기반 인가 설정
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/logout").authenticated()                          // 인증 받은 사용자만 접근 가능
                 .antMatchers("/**").permitAll()                                 // 인증/인가 여부와 상관 없이 접근 가능
-                .and()
-                    .logout()    // 로그아웃 설정
+                    .and()
+            .formLogin()                                                    // form login 인증 사용
+                    .and()
+            .logout()                                                       // 로그아웃 설정
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessHandler((req,res,auth) ->{
-                    res.setStatus(httpServletResponse.SC_UNAUTHORIZED);         // 로그아웃을 401로 응답
-                })
+                .logoutSuccessUrl("/")                                            // 로그아웃 성공 후 리다이렉트 될 URL
                 .invalidateHttpSession(true)                                      // 세션 무효화
-                .deleteCookies("JSESSIONID")                                      // JESSIONID
-                .permitAll()
-                .and()
-                .httpBasic();   //HTTP Basic 인증 사용
+                .deleteCookies("JSESSIONID")                                      // JESSIONID 쿠키 삭제
+                .permitAll();
         return http.build();
     }
 
