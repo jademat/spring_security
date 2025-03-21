85eeaf39f012aa74fa5d93248911b49923770584
jademat <01088049128a@gmail.com>
2025-03-21 09:40:09 +0900

    passwordEncoder 수정

--- src/main/java/com/example/jademat/spring_security/config/SecurityConfig.java
+++ src/main/java/com/example/jademat/spring_security/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -18,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    // SecurityFilterChain : 스프링 시큐리티에서 적용된 보얀규칙들을 필터로 구현해 둔 것
     @Bean
     public SecurityFilterChain FilterChain(HttpSecurity http, HttpServlet httpServlet, HttpServletResponse httpServletResponse) throws Exception {
         http
@@ -42,17 +45,23 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
+    public PasswordEncoder passwordEncoder() {  // 비밀번호 암호화에 사용할 인코더
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
                 .username("user")
-                .password("password")
+                .password(passwordEncoder.encode("password"))
                 .roles("USER")
                 .build();
 
-        UserDetails admin = User.withDefaultPasswordEncoder()
+        UserDetails admin = User.builder()
                 .username("admin")
-                .password("password")
-                .roles("ADMIN","USER")
+                .password(passwordEncoder.encode("password"))
+                // .roles("ADMIN","USER")
+                .roles("ADMIN") // ADMIN 권한 가짐
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
     }
